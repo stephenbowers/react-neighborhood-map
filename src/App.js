2fe2a40603c509ada45e44705f393e0f77120ebe
Stephen Bowers <srb4887@gmail.com>
2018-10-07 12:25:25 -0700

    feat: Add location ListView

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map';
+import ListView from './components/ListView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="listContainer">
+          <ListView />
+        </div>
+        <div className="mapContainer">
         <Map />
+        </div>
       </div>
     );
   }
---
+++ src/components/ListView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ListView extends Component {
+  render() {
+    return (
+      <div className="locationList">
+        <h1>Hillcrest Locations</h1>
+        <ul>
+            <li>Location</li>
+            <li>Location</li>
+            <li>Location</li>
+            <li>Location</li>
+            <li>Location</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ListView;
